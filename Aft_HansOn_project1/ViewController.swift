d49914eeaaa5f500aa0f348a687dd944b3ed6a18
riosudarsono <mas.riosudarsono@gmail.com>
2021-04-05 16:19:31 +0700

    Initial Commit

---
+++ Aft_HansOn_project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aft_HansOn_project1
+//
+//  Created by Rio Sudarsono on 05/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
